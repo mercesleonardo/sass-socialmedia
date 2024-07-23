18b71db3273e303eb44625d3630073b699ce21f5
Leonardo M Carvalho <leonardokarvalho@gmail.com>
2024-07-23 11:10:43 -0300

    Criação das migrations e models

---
+++ database/migrations/2024_07_23_135707_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('content_scheduling_id')->constrained('content_scheduling');
+            $table->foreignId('parent_id')->constrained('comments');
+            $table->string('name');
+            $table->text('comment');
+            $table->string('username');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
